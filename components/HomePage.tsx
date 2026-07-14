1ee3b454d3f0595206b157f7bf62bae8bc805eeb
Zeitgeist23 <JWigg023@gmail.com>
2026-07-13 20:20:43 -0500

    Update HomePage.tsx

--- components/HomePage.tsx
+++ components/HomePage.tsx
@@ -1,33 +1,14 @@
 "use client";
-import Image from "next/image";
 
 export default function HomePage() {
   return (
     <main className="exactVisualPage">
       <div className="visualCanvas">
-        <Image
+        <img
           src="/images/approved-homepage.png"
-          alt="Florida Liquor License Market homepage"
-          width={1024}
-          height={1536}
-          priority
+          alt="Florida Liquor License Market"
           className="approvedVisual"
         />
-        <a className="hotspot buyNav" href="#featured" aria-label="Buy" />
-        <a className="hotspot sellNav" href="mailto:info@floridaliquorlicensemarket.com?subject=Sell%20My%20License" aria-label="Sell" />
-        <a className="hotspot financeNav" href="mailto:info@floridaliquorlicensemarket.com?subject=Financing%20Inquiry" aria-label="Finance" />
-        <a className="hotspot investNav" href="mailto:info@floridaliquorlicensemarket.com?subject=Investment%20Inquiry" aria-label="Invest" />
-        <a className="hotspot marketNav" href="#market-data" aria-label="Market Data" />
-        <a className="hotspot resourcesNav" href="#resources" aria-label="Resources" />
-        <a className="hotspot listLicenseNav" href="mailto:info@floridaliquorlicensemarket.com?subject=List%20My%20License" aria-label="List your license" />
-        <a className="hotspot contactNav" href="mailto:info@floridaliquorlicensemarket.com" aria-label="Contact us" />
-        <a className="hotspot browseCard" href="#featured" aria-label="Browse featured licenses" />
-        <a className="hotspot sellCard" href="mailto:info@floridaliquorlicensemarket.com?subject=Sell%20My%20License" aria-label="Sell your license" />
-        <a className="hotspot financeCard" href="mailto:info@floridaliquorlicensemarket.com?subject=Financing%20Inquiry" aria-label="Explore financing" />
-        <a className="hotspot investCard" href="mailto:info@floridaliquorlicensemarket.com?subject=Investment%20Inquiry" aria-label="Investment opportunities" />
-        <div id="featured" className="anchor featuredAnchor" />
-        <div id="market-data" className="anchor marketAnchor" />
-        <div id="resources" className="anchor resourcesAnchor" />
       </div>
     </main>
   );
