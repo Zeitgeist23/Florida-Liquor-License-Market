6b76dd5daf3edd69c934687e6ead147e71d8321f
Zeitgeist23 <JWigg023@gmail.com>
2026-07-22 22:14:38 -0500

    Show uploaded audio on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,12 @@
 import HomePage from "@/components/HomePage";
+import MarketReportAudioPortal from "@/components/MarketReportAudioPortal";
 import "./home-market-insights.css";
 
 export default function Page() {
-  return <HomePage />;
+  return (
+    <>
+      <HomePage />
+      <MarketReportAudioPortal />
+    </>
+  );
 }
