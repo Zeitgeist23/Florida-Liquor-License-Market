c77b7640eda9d46bdcf7b965f5cf8d04da36523d
Zeitgeist23 <JWigg023@gmail.com>
2026-07-24 14:31:00 -0500

    Pass current marketplace inventory to homepage insights modal

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,17 @@
 import HomePage from "@/components/HomePage";
 import HomeCarouselAvailableColorFix from "@/components/HomeCarouselAvailableColorFix";
 import MarketReportAudioPortal from "@/components/MarketReportAudioPortal";
+import { getMarketplaceListings } from "@/lib/listing-store";
 import "./home-market-insights.css";
 
-export default function Page() {
+export const dynamic = "force-dynamic";
+
+export default async function Page() {
+  const marketplaceListings = await getMarketplaceListings();
+
   return (
     <>
-      <HomePage />
+      <HomePage marketListings={marketplaceListings} />
       <HomeCarouselAvailableColorFix />
       <MarketReportAudioPortal />
     </>
