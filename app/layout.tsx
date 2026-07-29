05c9ef917faafc50b46c3fbf2f3a5a8fafeef0ae
Zeitgeist23 <JWigg023@gmail.com>
2026-07-29 10:45:22 -0500

    Load ABT-6002 demographic dropdowns

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 
+import AbtDemographicSelects from "@/components/AbtDemographicSelects";
 import AbtIncreaseInSeriesSelect from "@/components/AbtIncreaseInSeriesSelect";
 
 import "./globals.css";
@@ -27,6 +28,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <AbtIncreaseInSeriesSelect />
+        <AbtDemographicSelects />
         {children}
       </body>
     </html>
