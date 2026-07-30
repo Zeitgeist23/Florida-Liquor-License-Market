1eec2f71d399be7213099c8b2c8d906e89d088b4
Zeitgeist23 <JWigg023@gmail.com>
2026-07-30 12:53:07 -0500

    Refresh Resources dropdown on forms pages

--- components/FormsSiteHeader.tsx
+++ components/FormsSiteHeader.tsx
@@ -36,7 +36,7 @@ export default function FormsSiteHeader() {
           </a>
         </div>
       </header>
-      <Script src="/assets/resources-dropdown.js?v=2" strategy="afterInteractive" />
+      <Script src="/assets/resources-dropdown.js?v=3" strategy="afterInteractive" />
     </>
   );
 }
