5594567ccc39ad58397fc1272c138272399f3a08
Zeitgeist23 <JWigg023@gmail.com>
2026-07-30 16:32:53 -0500

    Refresh Resources menu script

--- components/FormsSiteHeader.tsx
+++ components/FormsSiteHeader.tsx
@@ -36,7 +36,7 @@ export default function FormsSiteHeader() {
           </a>
         </div>
       </header>
-      <Script src="/assets/resources-dropdown.js?v=4" strategy="afterInteractive" />
+      <Script src="/assets/resources-dropdown.js?v=5" strategy="afterInteractive" />
     </>
   );
 }
