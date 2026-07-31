1dbc93faf54fe0538781b5d067b01c71f9cc4787
Zeitgeist23 <JWigg023@gmail.com>
2026-07-31 00:33:10 -0700

    Load current seller page design

--- app/sell-your-license/form/page.tsx
+++ app/sell-your-license/form/page.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 
 import SellerListingForm from "@/components/SellerListingForm";
+import "@/app/resources/forms/abt-forms.css";
+import "@/app/sell-your-license-preview/list-your-license-preview.css";
 
 export const metadata: Metadata = {
   title: "Confidential License Listing Form | Florida Liquor License Market",
