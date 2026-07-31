91768e676ed649a1d0fd73a829b4aedb3f64fa1c
Zeitgeist23 <JWigg023@gmail.com>
2026-07-30 21:40:32 -0700

    Refresh Resources dropdown fee page link

--- components/FormsSiteHeader.tsx
+++ components/FormsSiteHeader.tsx
@@ -36,7 +36,7 @@ export default function FormsSiteHeader() {
           </a>
         </div>
       </header>
-      <Script src="/assets/resources-dropdown.js?v=8" strategy="afterInteractive" />
+      <Script src="/assets/resources-dropdown.js?v=9" strategy="afterInteractive" />
     </>
   );
 }
