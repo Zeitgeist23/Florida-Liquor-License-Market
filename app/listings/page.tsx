2a969dde6cf6983aff18d714c7b205b220a6e129
Zeitgeist23 <JWigg023@gmail.com>
2026-08-10 16:09:01 -0700

    Load Listings filter depth refinement

--- app/listings/page.tsx
+++ app/listings/page.tsx
@@ -17,6 +17,7 @@ import "./listings-navy-refresh.css";
 import "./listings-card-gold-borders.css";
 import "./listings-title-highlight.css";
 import "./listings-regression-fix.css";
+import "./listings-filter-depth.css";
 
 const siteUrl = "https://www.floridaliquorlicensemarket.com";
 const listingsUrl = `${siteUrl}/listings`;
