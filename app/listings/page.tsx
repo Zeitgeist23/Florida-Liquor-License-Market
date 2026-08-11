fa0b5a71be3ed32583cf2ae80ff0ed4563edc4b8
Zeitgeist23 <JWigg023@gmail.com>
2026-08-10 17:15:34 -0700

    Load listing card overlap fix

--- app/listings/page.tsx
+++ app/listings/page.tsx
@@ -17,6 +17,7 @@ import "./listings-regression-fix.css";
 import "./listings-filter-depth.css";
 import "./listings-logo-3pct-lock.css";
 import "./listings-conversion-cards.css";
+import "./listings-card-overlap-fix.css";
 
 const siteUrl = "https://www.floridaliquorlicensemarket.com";
 const listingsUrl = `${siteUrl}/listings`;
