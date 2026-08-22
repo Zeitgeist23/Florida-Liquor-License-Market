487cae2bd4db0fb1be42ce346f42c0f268926fea
Zeitgeist23 <JWigg023@gmail.com>
2026-08-22 11:19:19 -0500

    Add Park Street findings reader pages 8-14

---
+++ data/court-documents/park-street-findings-pages-02.ts
@@ -0,0 +1,351 @@
+export const PARK_STREET_FINDINGS_PAGES_02 = [
+`would enable the Park Street Trust to submit an application, sometime after the Closing, to the
+
+DABT to facilitate the transfer of the license interest of the License. T.IV:490:4-25; T.IV:491:7-
+
+23. The relevant application form, the ABT Form 6002, is not a part of the closing documents and
+
+therefore the transfer of the license interest has nothing to do with the transfer of the property
+
+interest that is the subject of the Agreement; the transfer of the license interest is outside of the
+
+scope of the Agreement. TTE 4. In fact, the Park Street Trust would never actually obtain the
+
+license interest of the License as it would assign its rights to Winn-Dixie, which provides further
+
+support as to why the transfer of the license interest was never part of the Agreement. T.IV.492:1-7.
+
+       Following the execution of the Agreement and acting within his role as the broker, Mr.
+
+Rosayn commenced due diligence on the license and its ownership. T.II.212:8-19. Through due
+
+diligence, Mr. Rosayn discovered issues with the license encumbering the ownership interest and
+
+the transferability of the License, issues that were impediments to Closing. T.II.215:9-15.
+
+Specifically, Mr. Rosayn discovered the following impediments: (i) sales tax issues; (11)
+
+reemployment tax issues; (iil) missing returns; (iv) the “no-sale list” status of the License; (v)
+
+foreclosure; and (vi) the License renewal issue. T.II.215:9-15. Curing these impediments were all
+
+obligations of the Seller under the Agreement. T.II.225:20-25; T.II.226:1-3.
+
+       Mr. Rosayn conducted a lien search with DABT and conducted a public records search to
+
+determine if there was any pending litigation or judgments, judgment liens, or UCC-1 financing
+
+statements encumbering the License. T.II.212:8-19.
+
+       Mr. Rosayn provided Mr. Wigg with a Florida Department of Revenue DR-835 Power of
+
+Attorney (“DOR POA”), which enabled Mr. Rosayn to communicate with the Florida Department
+
+of Revenue (“DOR”) and obtain the information necessary and required to determine if there were
+
+outstanding tax liabilities or if there were any issues that needed to be resolved as well as to obtain
+
+
+
+                                                  8`,
+`sales tax records in order to compute the “quota license transfer fee,” and other sales tax
+
+documentation that may be necessary. T.II.212:22-25; T.II.213:1-10.
+
+       Upon the return by Mr. Wigg of the executed POA by Nieves Wigg, Mr. Wigg’s mother,
+
+Mr. Rosayn submitted the DOR POA to the DOR to obtain a “certificate of compliance.”
+
+T.11.212:22-25; T.I1.213:1-10. The DOR responded with a denial of the certificate of compliance
+
+because there were a number of issues related to reemployment tax, sales tax, missing returns, and
+
+unpaid bills. T.I1.212:22-25; T.II.213:1-10. Mr. Rosyan provided Mr. Wigg a copy of the denials
+
+of the certificate of compliance and the judgment liens that appeared in the public records that
+
+were from the DOR. T.1II.215:21-25.
+
+
+       Mr. Rosayn also discovered that the license was being foreclosed for failure to meet
+
+obligations under a purchase money security agreement related to a loan from a group called
+
+Provantage Group and arranged the attorney Marc Tiller. T.I1.212:22-25; T.11.213:110. Mr.
+
+Rosayn contacted Mr. Tiller about the foreclosure, and Mr. Tiller informed Mr. Rosayn that the
+
+foreclosure was based on Beachway’s monetary defaults. T.II.213:16-22.
+
+       In his broker role, to facilitate the Closing, Mr. Rosayn assisted Mr. Wigg in an effort to
+
+resolve some of the Seller’s obligations, and to try to get a payoff estoppel letter from the mortgage
+
+holder, Provantage Group. T.II.223:23-25; T.II.224:1-15; TTE 8. In fact, after the execution of the
+
+Agreement, Mr. Rosayn and Mr. Wigg spent the following three months working together to “try
+
+and sort out the mess of sales tax filings and payments that were needed to clear the license for
+
+sale.” TTE 8.
+
+
+       Mr. Rosayn discovered that the License was on the “no-sale list” because some wholesale
+
+venders had reported Beachway for failure to pay the bills within the statutory time frame.
+
+T.11.214:2-7. Mr. Rosayn discovered that there were tax liens recorded both with the Florida
+
+
+
+                                                  9`,
+`Secretary of State as judgment liens and also in St. Johns County with the Clerk of the Court.
+
+T.II.214:2-7. Mr. Rosayn also discovered that the License had not been renewed. T.I.215:11-15.
+
+       Mr. Rosayn utilized the DOR POA to request payoff letters, estoppels, and other
+
+information from the DOR in order to work with Mr. Wigg to satisfy these Seller obligations.
+
+T.I1.223:23-25;     T.11.224:1-9.vAfter each and every item was uncovered, Mr.                 Rosayn
+
+communicated with Shutts and solicited their opinions on the matters, and communicated and
+
+provided Mr. Wigg with everything that Mr. Rosayn uncovered. T.II.214:2-19; T.I.220:13-16.
+
+After discussing each of these items with Mr. Wigg, Mr. Wigg made several representations as to
+
+his efforts, ability, and willingness to cure these issues, including the following:
+
+
+            1. Mr. Wigg represented that he was aware of the issue with the foreclosure and was
+
+               attempting to communicate with Mr. Tiller to resolve the foreclosure, but was
+
+               unable to do so. T.II.214:9-19.
+
+
+
+           2. Mr. Wigg represented that he was going to coordinate a payment or had made a
+
+               payment of some of the “no-sale list” items, in an effort to cure the issue. T.II.214:9-
+
+                  19,
+
+
+           3. Mr. Wigg represented that he was going to coordinate a payment of one of the
+
+               outstanding sales tax liens. T.II.214:9-19.
+
+           4. Mr. Wigg represented that he would contact the payroll company to get additional
+
+               information to address the reemployment tax liens (together with the sales tax liens,
+
+               the “Tax Lien” T.II.214:9-19.
+
+
+During this time, Mr. Rosayn remained in contact with the DOR and provided the DOR with
+
+documents given to him by Mr. Wigg in an effort to clear the outstanding Tax Lien. .II.214:23-25;
+
+T.II.215:1-5. Mr. Rosayn specifically asked Mr. Wigg if he was able to delinquently renew the
+
+                                                  10`,
+`liquor license. T.II.220:22-25; T.II.221:1-6. Mr. Wigg would ultimately coordinate having the
+
+delinquent renewal fee paid. T.II.225:11-15. Mr. Rosayn specifically asked Mr. Wigg about the
+
+Tax Lien, and Mr. Wigg represented that he was working to get a number of the items to Mr.
+
+Rosayn and the DOR, as Mr. Wigg was actively negotiating a reduction of the Tax Lien.
+
+T.I1.220:22-25; T.11.221:1-6. As a result of Mr. Wigg’s representation, coupled with the fact that
+
+Mr. Rosayn was not made a part of the negotiation process with the DOR, Mr. Rosayn relied on
+
+Mr. Wigg to provide the final amount of the negotiated Tax Lien. T.IV.664:15-18; T.IV.666:8-10.
+
+       Once Mr. Wigg provided Mr. Rosayn some of the deliverables that Mr. Wigg represented
+
+he would provide to Mr. Rosayn, Mr. Rosayn forwarded them on to the DOR and again applied
+
+for a certificate of compliance. T.1.220:22-25; T.II.221:1-6. However, the second certificate of
+
+compliance was denied as there were a number of remaining issues that had not yet been resolved.
+
+T.IL.220:22-25; T.1L.221:1-6.
+
+       As a result of Beachway’s impediments to closing, and at the request of the Beachway, on
+
+or about February 9, 2021, the Park Street Trust and Beachway entered into an Amendment to the
+
+Asset Purchase Agreement (“Amendment”) whereby the Park Street Trust agreed to modify the
+
+Agreement to extend its Closing Date. TTE 2. The Closing date was thereby modified in the
+
+Amendment to take place on or before February 26, 2021 (‘Amended Closing Date”), with
+
+additional provision that the Amended Closing Date could be extended by up to five (5) business
+
+days upon the written request of either Party with all other material provisions of the Agreement
+
+remaining the same. TTE 2. The Amendment was validly executed by the Defendant, as the
+
+signatures of Mr. Wigg, as Authorized Member, and Nieves Wigg, as Manager, clearly appear on
+
+the face of the Amendment on behalf of Beachway. TTE 2.
+
+
+
+
+                                               11`,
+`       The purpose of the Amendment was to extend the diligence period prior to Closing to
+
+provide Mr. Wigg with additional time to resolve outstanding issues related to the License and its
+
+ownership, including, importantly, the negotiation and resolution of the outstanding Tax Lien.
+
+T.II.226:13-24. Mr. Rosayn continued to work with Mr. Wigg to try to assist with the unresolved
+
+Seller obligations related to Closing. T.II.227:5-15; TTE 8. Again with no other liquor licenses
+
+available, Mr. Wigg was proverbially the “only show in town” so the trust needed to keep working
+
+with him.
+
+
+       Prior to the end of the business day on the Amended Closing Date, the Park Street Trust,
+
+once again, accommodated Beachway by providing it with yet another extension so that it could
+
+negotiate, or otherwise satisfy the Tax Lien, and requested (“Second Amended Closing Date
+
+Request”) that the Amended Closing Date be extended the extra five (5) business days provided
+
+for in the Amendment from February 26, 2021 to March 5, 2021 (‘Second Amended Closing
+
+Date”). T.1.115:11-18; TTE 3; TTE 8. In the email memorializing this, the Park Street Trust
+
+specifically noted that “Buyer hereby requests to extend the Closing Date the additional 5 business
+
+days provided under Section 3, to allow Seller the opportunity to finalize its resolution of all
+
+matters affecting transferability of the license, including the DOR tax liability, and lien/lawsuit
+
+matters related to the St. Johns mortgage.” TTE 3.
+
+       Between February 26, 2021, and March 5, 2021, the Park Street Trust prepared for Closing.
+
+T.1.116:11-22. Shutts prepared the closing documents, which included the bill of sale, the Seller’s
+
+no lien affidavit, the Beachway’s company resolution, DABT Affidavit of Transfer, and a closing
+
+statement. T.1.116:11-22. The closing statement did not include the Tax Lien figure because
+
+Beachway failed to provide Mr. Rosayn or the Park Street Trust with the Tax Lien figure.
+
+T.1.116:11-22. Beachway represented that it was actively negotiating with the DOR and therefore,
+
+
+
+                                                12`,
+`only Beachway could ever know what the Tax Lien figure was. T.I.105:7-24; T.1.156:22-25.
+
+Knowing that the Tax Lien figure would be withheld from the gross proceeds of the sale thereby
+
+reducing the net proceeds to Beachway, Beachway represented that it was attempting to negotiate
+
+the Tax Lien figure down. T.I.105:7-24; T.1.156:22-25. At Closing, the amount of the Tax Lien
+
+was Set be deducted and paid to the Department of Revenue, meaning that upon the imputation of
+
+the negotiated Tax Lien figure and the Closing of the transaction, the impediment would be
+
+resolved. T.I.105:7-24; T.I.156:22-25. The negotiated Tax Lien figure was solely in Beachway’s
+
+possession and Beachway knew what the negotiated Tax Lien figure would be to release the lien.
+
+T.I.116:19-22. Mr. Rosayn and Winn-Dixie had no way of determining the negotiated Tax Lien
+
+even with the DOR POA, as they were not involved with negotiating the Tax Lien. Beachway
+
+failed to provide that critical information to the Park Street Trust or Mr. Rosayn in the manner in
+
+which Beachway had provided other information during the due diligence of this transaction, even
+
+though providing clean title was an obligation of the Seller under the Agreement. T.I.116:19-22.
+
+Thus, as the Closing Date approached, the only issue that remained was satisfaction of the Tax
+
+Lien, which was being negotiated solely by Beachway and was exclusively the Seller’s obligation.
+
+T.1.13:22-25; T.1L.227:20-25; T.I.228:1-3.
+
+       On March 4, 2021 at 4:33 PM, Sue VanLeeuwen, a Shutts attorney, sent Mr. Rosayn an
+
+email containing PDF versions of the revised closing documents as provided for in the Agreement,
+
+as amended. TTE 4; T.I.229:8-13. The email also stated that “[t]he Cash to Close funds have been
+
+submitted by Buyer to, and receipt has been confirmed by, RealtyMasters. Therefore, Buyer is
+
+ready, willing and able to close on this purchase as scheduled tomorrow, Friday, March 5, 2021,
+
+and fully expects Seller to comply with the valid terms of the Asset Purchase Agreement, as
+
+amended . . . if you have any questions, or modifications to the attached, please let me know.” TTE
+
+
+
+                                                13`,
+`4. Mr. Rosayn testified that “[Shutts] were coordinating the cash to close as -- and I had confirmed
+
+it as the escrow agent, and I forwarded all of the closing documents to Mr. Wigg.” T.II.229:21-25;
+
+T.II.230:1; TTE -230; Trust E. On March 4, 2021, at 5:23 PM, Mr. Rosayn forwarded the March
+
+4, 2021 email from Mrs. VanLeeuwen to Mr. Wigg, notifying Mr. Wigg that the Buyer wanted to
+
+close. T.II.228:4-10; TTE 5. On the same day, Mr. Rosayn tried numerous times to get in touch
+
+with Mr. Wigg via phone and text so that Mr. Rosayn could review the documents ahead of the
+
+Closing. Trust E. Mr. Rosayn was unable to reach Mr. Wigg. TTE 5. Later at 6:51 PM, Mr. Wigg
+
+replied to Mr. Rosayn’s forwarding email® and informed Mr. Rosayn that he was having stock
+
+market issues and other personal financial issues, and he could not complete the closing.
+
+T.II.230:3-6. Also in that email, Mr. Wigg emphatically stated, “I do not want the first quarter
+
+funds that are claimed by DOR deducted or paid. We cannot close until this is resolved[,
+                                                                                       |” despite
+
+the resolution of the Tax Lien being Beachway’s obligation at Closing.                    TTE 5. This email
+
+represents the final communication from Mr. Wigg until after the lawsuit was filed four days later,
+
+despite Mr. Rosayn’s best efforts to get in touch with Mr. Wigg. T.IV.663:20-25; T.IV.664:1-8.
+
+Mr. Wigg’s failure to respond to Mr. Rosayn or return any of his calls on the day of Closing was
+
+“extremely” unusual behavior from a Seller. T.1V.663:20-25; T.IV.664:1-8.
+
+        Following Mr. Wigg’s email, Mr. Rosayn, Mr. Gillis, and Mrs. VanLeeuwen exchanged
+
+emails discussing the possibility of offering Mr. Wigg a second amendment to the Agreement to
+
+extend the closing date to allow Mr. Wigg to resolve his last issue. TTE 10. On March 4, 2021, at
+
+10:36 PM, Mrs. VanLeeuwen sent an email to Mr. Gillis and Mr. Rosayn with a “draft Second
+
+
+
+
+6 Mr. Wigg denies ever having received the closing documents and ever having seen the March 4, 2021, 4:33 PM
+email from Mrs. VanLeeuwen to Mr. Rosayn, despite having replied to that very email after it was forwarded to him
+(as shown in both TTE 5 and TTE 10). T.IV. 573:10-18.
+
+
+                                                       14`,
+] as const;
