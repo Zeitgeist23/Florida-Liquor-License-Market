520c1a99c10ccd28dcd5b9ac1f366735164e4cfd
Zeitgeist23 <JWigg023@gmail.com>
2026-08-31 10:04:11 -0500

    Add buy page route styling layout

---
+++ app/buy-florida-liquor-license/layout.tsx
@@ -0,0 +1,6 @@
+import type { ReactNode } from "react";
+import "./buy-page.css";
+
+export default function BuyFloridaLiquorLicenseLayout({ children }: { children: ReactNode }) {
+  return children;
+}
