8653469428b34b70b3d91c53be1772436ce078ce
Zeitgeist23 <JWigg023@gmail.com>
2026-09-04 18:07:50 -0500

    Point Featured modal to verified image asset

--- components/BrokerSampleModalLink.tsx
+++ components/BrokerSampleModalLink.tsx
@@ -6,7 +6,7 @@ import styles from "./BrokerSampleModalLink.module.css";
 type Tier = "standard" | "featured";
 
 const STANDARD_SAMPLE_PAGE = "/brokers/sample-standard-listing";
-const FEATURED_SAMPLE_IMAGE = "/assets/brokers/fllm-featured-broker-sample-v2.webp";
+const FEATURED_SAMPLE_IMAGE = "/assets/brokers/fllm-featured-broker-sample-v3.webp";
 
 export default function BrokerSampleModalLink({ tier }: { tier: Tier }) {
   const [open, setOpen] = useState(false);
