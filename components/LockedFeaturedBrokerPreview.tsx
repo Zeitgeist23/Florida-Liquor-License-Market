ba25db12a759dec39b8c89098ef497dfddd4f868
Zeitgeist23 <JWigg023@gmail.com>
2026-09-06 06:19:58 -0500

    Replace broken female broker portrait with working JPG asset

--- components/LockedFeaturedBrokerPreview.tsx
+++ components/LockedFeaturedBrokerPreview.tsx
@@ -7,7 +7,7 @@ const SAMPLE_BROKERS = {
   female: {
     name: "Emma Brooks",
     email: "emma.brooks@listingbroker.com",
-    image: "/assets/brokers/fllm-featured-broker-sample-sharp.webp",
+    image: "/assets/brokers/fllm-featured-broker-preview.jpg",
     alt: "Fictitious female independent listing broker",
   },
   male: {
