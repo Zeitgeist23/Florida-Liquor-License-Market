a94b7ea5cdf3cc5dd726e599e423000d80391e92
Zeitgeist23 <JWigg023@gmail.com>
2026-09-07 16:30:51 -0500

    Assemble exact approved Exchange news asset

---
+++ app/market-data/exchange-board/news-data.ts
@@ -0,0 +1,10 @@
+import c0 from "./news-chunk-0";
+import c1 from "./news-chunk-1";
+import c2 from "./news-chunk-2";
+import c3 from "./news-chunk-3";
+import c4 from "./news-chunk-4";
+import c5 from "./news-chunk-5";
+
+const newsDataUri = `data:image/webp;base64,${c0}${c1}${c2}${c3}${c4}${c5}`;
+
+export default newsDataUri;
