80efa07d369f68bdebd236c17ce76a36291f4916
Zeitgeist23 <JWigg023@gmail.com>
2026-09-07 00:11:18 -0500

    Add Exchange Board to Market Data navigation

--- components/HeaderNavMenus.tsx
+++ components/HeaderNavMenus.tsx
@@ -73,6 +73,7 @@ const navMenus: NavMenu[] = [
     label: "Market Data",
     menuClassName: "native-nav-menu-standard native-nav-market-menu",
     links: [
+      { href: "/market-data/exchange-board", label: "FLLM Exchange Board" },
       { href: "/counties", label: "Florida Market Data by County" },
       { href: "/florida-liquor-license-value", label: "Florida Liquor License Value Estimator" },
       { href: "/florida-quota-liquor-license-cost", label: "Florida Liquor License Cost by County" },
