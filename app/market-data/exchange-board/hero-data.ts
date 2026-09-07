bd00b5bc5a35f679f2e0bbf354f05441a80ce6d8
Zeitgeist23 <JWigg023@gmail.com>
2026-09-07 16:18:49 -0500

    Assemble exact approved Exchange hero asset

---
+++ app/market-data/exchange-board/hero-data.ts
@@ -0,0 +1,12 @@
+import c0 from "./hero-chunk-0";
+import c1 from "./hero-chunk-1";
+import c2 from "./hero-chunk-2";
+import c3 from "./hero-chunk-3";
+import c4 from "./hero-chunk-4";
+import c5 from "./hero-chunk-5";
+import c6 from "./hero-chunk-6";
+import c7 from "./hero-chunk-7";
+
+const heroDataUri = `data:image/webp;base64,${c0}${c1}${c2}${c3}${c4}${c5}${c6}${c7}`;
+
+export default heroDataUri;
