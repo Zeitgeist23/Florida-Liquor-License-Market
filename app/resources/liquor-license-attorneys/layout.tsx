3dd198aa176b11fcd45e129f198e995313f29e0e
Zeitgeist23 <JWigg023@gmail.com>
2026-09-09 14:00:21 -0500

    Load attorney modal portrait fixes

---
+++ app/resources/liquor-license-attorneys/layout.tsx
@@ -0,0 +1,6 @@
+import type { ReactNode } from "react";
+import "./sutton-modal-fix.css";
+
+export default function LiquorLicenseAttorneysLayout({ children }: { children: ReactNode }) {
+  return <>{children}</>;
+}
