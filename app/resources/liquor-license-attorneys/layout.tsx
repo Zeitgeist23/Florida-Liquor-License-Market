5475b124030ecc5ec3b3a65ac7241cab73f87c81
Zeitgeist23 <JWigg023@gmail.com>
2026-09-09 18:33:06 -0500

    Remove stale Sutton modal override stylesheet

--- app/resources/liquor-license-attorneys/layout.tsx
+++ app/resources/liquor-license-attorneys/layout.tsx
@@ -1,5 +1,4 @@
 import type { ReactNode } from "react";
-import "./sutton-modal-fix.css";
 
 export default function LiquorLicenseAttorneysLayout({ children }: { children: ReactNode }) {
   return <>{children}</>;
