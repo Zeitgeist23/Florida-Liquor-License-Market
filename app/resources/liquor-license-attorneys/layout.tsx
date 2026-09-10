865388aca2183d3991e0a6dc00641cbb9d66a61a
Zeitgeist23 <JWigg023@gmail.com>
2026-09-09 19:51:40 -0500

    Use approved Sutton static modal replacement styling

--- app/resources/liquor-license-attorneys/layout.tsx
+++ app/resources/liquor-license-attorneys/layout.tsx
@@ -1,5 +1,5 @@
 import type { ReactNode } from "react";
-import "./sutton-static-modal.css";
+import "./sutton-static-replacement.css";
 
 export default function LiquorLicenseAttorneysLayout({ children }: { children: ReactNode }) {
   return <>{children}</>;
