234778180bd3d123a3c4920135f5298ef287de1d
Zeitgeist23 <JWigg023@gmail.com>
2026-09-09 19:28:46 -0500

    Replace Sutton popup with approved static image

--- app/resources/liquor-license-attorneys/layout.tsx
+++ app/resources/liquor-license-attorneys/layout.tsx
@@ -1,4 +1,5 @@
 import type { ReactNode } from "react";
+import "./sutton-static-modal.css";
 
 export default function LiquorLicenseAttorneysLayout({ children }: { children: ReactNode }) {
   return <>{children}</>;
