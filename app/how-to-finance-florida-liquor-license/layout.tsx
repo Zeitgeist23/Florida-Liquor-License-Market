49ca56ce7ab9bbaeb613c20aa56baddaf8821391
Zeitgeist23 <JWigg023@gmail.com>
2026-09-15 21:21:43 -0500

    Apply robust financing title centering and FAQ open-state behavior

--- app/how-to-finance-florida-liquor-license/layout.tsx
+++ app/how-to-finance-florida-liquor-license/layout.tsx
@@ -1,4 +1,5 @@
 import type { ReactNode } from "react";
+import FinanceGuideBehaviorFixes from "./FinanceGuideBehaviorFixes";
 import "./lining-numerals.css";
 import "./financing-snapshot-cards.css";
 import "./financing-terms-cards.css";
@@ -6,5 +7,10 @@ import "./finance-guide-link-cards.css";
 import "./faq-open-gold.css";
 
 export default function HowToFinanceLayout({ children }: { children: ReactNode }) {
-  return <div className="finance-guide-lining-numerals">{children}</div>;
+  return (
+    <div className="finance-guide-lining-numerals">
+      {children}
+      <FinanceGuideBehaviorFixes />
+    </div>
+  );
 }
