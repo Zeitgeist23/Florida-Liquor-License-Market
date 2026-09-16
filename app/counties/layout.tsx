83d01c0966efa06a9211fb11631ae9dc29ac7d4f
Zeitgeist23 <JWigg023@gmail.com>
2026-09-15 23:29:38 -0500

    Add county heat map viewport fix

--- app/counties/layout.tsx
+++ app/counties/layout.tsx
@@ -1,7 +1,13 @@
 import type { ReactNode } from "react";
+import CountyHeatMapViewportFix from "@/components/CountyHeatMapViewportFix";
 import "./counties-heading-center.css";
 import "./county-table-row-hover.css";
 
 export default function CountiesLayout({ children }: { children: ReactNode }) {
-  return children;
+  return (
+    <>
+      <CountyHeatMapViewportFix />
+      {children}
+    </>
+  );
 }
