783231c041db6dcfe30b6efce7738c006e28c902
Zeitgeist23 <JWigg023@gmail.com>
2026-09-16 11:41:09 -0500

    Load Antezza featured detail badge styles

---
+++ app/listings/fllm-antezza/layout.tsx
@@ -0,0 +1,7 @@
+import type { ReactNode } from "react";
+
+import "./featured-detail-badge.css";
+
+export default function AntezzaListingLayout({ children }: { children: ReactNode }) {
+  return children;
+}
