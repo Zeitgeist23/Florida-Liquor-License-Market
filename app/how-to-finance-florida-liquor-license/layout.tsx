ab02e5ba5bd79d36fe7395e498dc75e9433c6b9b
Zeitgeist23 <JWigg023@gmail.com>
2026-09-15 20:59:28 -0500

    Use lining numerals on financing guide

---
+++ app/how-to-finance-florida-liquor-license/layout.tsx
@@ -0,0 +1,6 @@
+import type { ReactNode } from "react";
+import "./lining-numerals.css";
+
+export default function HowToFinanceLayout({ children }: { children: ReactNode }) {
+  return <div className="finance-guide-lining-numerals">{children}</div>;
+}
