99de2dad912079aa1cf81558b4cb0e4ba2f81a29
Zeitgeist23 <JWigg023@gmail.com>
2026-09-15 22:20:35 -0500

    Load counties heat map heading alignment

---
+++ app/counties/layout.tsx
@@ -0,0 +1,6 @@
+import type { ReactNode } from "react";
+import "./counties-heading-center.css";
+
+export default function CountiesLayout({ children }: { children: ReactNode }) {
+  return children;
+}
