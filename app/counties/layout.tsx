0bd2b5963a4a5ebccb16fcdccf69c09df4c4f8d9
Zeitgeist23 <JWigg023@gmail.com>
2026-09-15 22:57:55 -0500

    Apply luminous county table row hover effect

--- app/counties/layout.tsx
+++ app/counties/layout.tsx
@@ -1,5 +1,6 @@
 import type { ReactNode } from "react";
 import "./counties-heading-center.css";
+import "./county-table-row-hover.css";
 
 export default function CountiesLayout({ children }: { children: ReactNode }) {
   return children;
