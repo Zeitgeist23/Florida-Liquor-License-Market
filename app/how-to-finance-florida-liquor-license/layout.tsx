3bebb2a823be4129f778528661f430e1f473c211
Zeitgeist23 <JWigg023@gmail.com>
2026-09-15 21:11:54 -0500

    Load open FAQ gold title styling

--- app/how-to-finance-florida-liquor-license/layout.tsx
+++ app/how-to-finance-florida-liquor-license/layout.tsx
@@ -3,6 +3,7 @@ import "./lining-numerals.css";
 import "./financing-snapshot-cards.css";
 import "./financing-terms-cards.css";
 import "./finance-guide-link-cards.css";
+import "./faq-open-gold.css";
 
 export default function HowToFinanceLayout({ children }: { children: ReactNode }) {
   return <div className="finance-guide-lining-numerals">{children}</div>;
