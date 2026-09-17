ea22b5eb28604c3eec707eb9207ab914fc7b1d7e
Zeitgeist23 <JWigg023@gmail.com>
2026-09-17 13:36:37 -0500

    Apply broker page alignment fixes

---
+++ app/florida-liquor-license-broker/layout.tsx
@@ -0,0 +1,7 @@
+import type { ReactNode } from "react";
+
+import "./broker-page-fix.css";
+
+export default function FloridaLiquorLicenseBrokerLayout({ children }: { children: ReactNode }) {
+  return children;
+}
