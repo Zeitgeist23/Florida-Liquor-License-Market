37bfdd4fc50c59528d9bb0e81d49c8cdea16b69a
Zeitgeist23 <JWigg023@gmail.com>
2026-09-16 19:33:39 -0500

    Load targeted table layout fixes on license types page

---
+++ app/resources/florida-liquor-license-types/layout.tsx
@@ -0,0 +1,6 @@
+import type { ReactNode } from "react";
+import "./table-layout-fixes.css";
+
+export default function FloridaLiquorLicenseTypesLayout({ children }: { children: ReactNode }) {
+  return children;
+}
