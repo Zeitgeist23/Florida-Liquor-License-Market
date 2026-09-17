0ee3e6c5edb39a5b9d76c298b71eaa14b4801ba9
Zeitgeist23 <JWigg023@gmail.com>
2026-09-17 11:51:02 -0500

    Show broker outreach email preview on admin page

--- app/admin/broker-outreach/page.tsx
+++ app/admin/broker-outreach/page.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 
 import AdminBrokerOutreachClient from "./AdminBrokerOutreachClient";
+import BrokerEmailMasterPreview from "./BrokerEmailMasterPreview";
 import "./broker-outreach.css";
 
 export const metadata: Metadata = {
@@ -11,5 +12,10 @@ export const metadata: Metadata = {
 export const dynamic = "force-dynamic";
 
 export default function Page() {
-  return <AdminBrokerOutreachClient />;
+  return (
+    <>
+      <AdminBrokerOutreachClient />
+      <BrokerEmailMasterPreview />
+    </>
+  );
 }
