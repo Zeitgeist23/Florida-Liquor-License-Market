0ca6f39ff733ab690cdf5a3206f855ad4f7cd42b
Zeitgeist23 <JWigg023@gmail.com>
2026-09-18 12:10:34 -0500

    Add Julie featured broker mockup layout

---
+++ app/listings/fllm-julie-mockup/layout.tsx
@@ -0,0 +1,7 @@
+import type { ReactNode } from "react";
+
+import "../third-party-business-listing-standard.css";
+
+export default function JulieMockupLayout({ children }: { children: ReactNode }) {
+  return children;
+}
