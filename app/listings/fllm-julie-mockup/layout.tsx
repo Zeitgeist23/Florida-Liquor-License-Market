62ae27828b3d6cd05ab8d3bc0396acdd3b103316
Zeitgeist23 <JWigg023@gmail.com>
2026-09-18 12:33:25 -0500

    Remove unintended Julie mockup page

--- app/listings/fllm-julie-mockup/layout.tsx
+++
@@ -1,7 +0,0 @@
-import type { ReactNode } from "react";
-
-import "../third-party-business-listing-standard.css";
-
-export default function JulieMockupLayout({ children }: { children: ReactNode }) {
-  return children;
-}
