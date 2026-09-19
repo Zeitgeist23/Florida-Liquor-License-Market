28541ff9ba2e0f0d6a2093fe199e7f257e8d57db
Zeitgeist23 <JWigg023@gmail.com>
2026-09-18 22:07:03 -0500

    Add clear JR DesAmours portrait to preview

--- app/listings/fllm-desamours/page.tsx
+++ app/listings/fllm-desamours/page.tsx
@@ -72,6 +72,7 @@ const config: FeaturedThirdPartyBusinessListingConfig = {
     website: "https://myexitplan.com/broker/j-r-desamours/",
     listingUrl:
       "https://www.bizbuysell.com/business-opportunity/iconic-jensen-beach-bar-for-sale-w-4-cop-license/2486902/",
+    photo: "/assets/jr-desamours-clear.jpg",
   },
   additionalSellerIntro:
     "Opportunity to acquire a long-established neighborhood bar in Jensen Beach, Florida, together with its highly sought-after Martin County 4COP quota liquor license through an asset-sale structure.",
