b4a0183b9eaa8f941246858703e7d54fd5e5eb77
Zeitgeist23 <JWigg023@gmail.com>
2026-09-22 13:36:49 -0700

    Align generated taxpayer telephone field height

--- scripts/create_fillable_dr835.py
+++ scripts/create_fillable_dr835.py
@@ -103,7 +103,7 @@ def page_one_fields(form):
     add_text_field(form, "federal_identification_numbers", 484, 232, 695, 260)
     add_text_field(form, "florida_tax_registration_numbers", 707, 232, 955, 260)
     add_text_field(form, "taxpayer_contact_person", 484, 303, 695, 320, font_size=7)
-    add_text_field(form, "taxpayer_telephone", 803, 282, 955, 295, font_size=7)
+    add_text_field(form, "taxpayer_telephone", 803, 280, 955, 297, font_size=7)
     add_text_field(form, "taxpayer_fax", 790, 312.3, 955, 327.3, font_size=7)
 
     # Section 2 - Representatives
