0eb3ad760631c261d710c97cc9fb3b7525faf2bf
Zeitgeist23 <JWigg023@gmail.com>
2026-09-25 10:33:30 -0700

    Register restaurant hub for official FLLM footer

--- components/SellPageOfficialFooter.tsx
+++ components/SellPageOfficialFooter.tsx
@@ -24,6 +24,7 @@ const officialFooterPaths = new Set([
   "/exchange",
   "/transaction-services",
   "/businesses-with-quota-licenses/bars",
+  "/restaurants-with-liquor-licenses",
   "/florida-liquor-license-market-platform",
   "/are-florida-quota-liquor-licenses-worth-it",
 ]);
